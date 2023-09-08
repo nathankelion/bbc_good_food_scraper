85519483ffc1c318d2a65146c8999137b1ac28bc
nathankelion <nathankelion26@gmail.com>
2023-09-08 16:23:28 +0100

    improved the reproducibility of this function

--- scripts/get_recipe_links.py
+++ scripts/get_recipe_links.py
@@ -3,7 +3,7 @@ def get_recipe_links(soup, base_url):
     recipe_links_list = []
     recipe_ext = soup.find_all('a', attrs={'class':'link d-block'})
     for ext in recipe_ext:
-        recipe_url = base_url + ext['href']
-        recipe_links_list.append(recipe_url)
-    recipe_links_list = recipe_links_list[:-3]
+        if '/recipes/' in ext['href']:
+            recipe_url = base_url + ext['href']
+            recipe_links_list.append(recipe_url)
     return recipe_links_list
