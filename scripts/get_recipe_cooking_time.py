6a07d3ed3062189ac51aa8106add107a86852456
nathankelion <nathankelion26@gmail.com>
2023-09-12 10:38:13 +0100

    rewritten get_recipe_cooking_times function to allow handle time ranges

--- scripts/get_recipe_cooking_time.py
+++ scripts/get_recipe_cooking_time.py
@@ -1,45 +1,62 @@
 import re
+from lxml import html
 
 # Get cooking time
-def get_recipe_cooking_time(soup):
-    # Find all <time> elements with a 'datetime' attribute
-    cooking_times = soup.find_all('time', attrs={'datetime': lambda value: value is not None})
-    raw_list = []
-    cooking_times_HH_mm = []
+def get_recipe_cooking_time(response):
+    # Convert the soup to an lxml element tree
+    tree = html.fromstring(response.content)
     
-    # Extract and print the text content of each element
-    for time_element in cooking_times:
-        raw_list.append(time_element.text)
+    # Locate the cook and prep times
+    prep_times = tree.xpath('/html/body/div/div[4]/main/div[2]/section/div/div[4]/ul[1]/li[1]/div/div[2]/ul/li[1]/span[2]/time')
+    cook_times = tree.xpath('/html/body/div/div[4]/main/div[2]/section/div/div[4]/ul[1]/li[1]/div/div[2]/ul/li[2]/span[2]/time')
 
-    for time_str in raw_list:
+    cook = []
+    prep = []
+    cook_HH_mm = []
+    prep_HH_mm = []
+    
+    # Extract and print the text content of cook element(s)
+    for time_element in cook_times:
+        cook.append(time_element.text)
+
+    # Extract and print the text content of prep element(s)
+    for time_element in prep_times:
+        prep.append(time_element.text)
+
+    # Function to convert a time string to hours and minutes
+    def get_hours_and_minutes(time_str):
         hours, minutes = 0, 0
-        
-        # Check for hours and extract the numeric value
         match_hr = re.search(r'(\d+) hr', time_str)
         if match_hr:
             hours = int(match_hr.group(1))
-        
-        # Check for minutes and extract the numeric value
         match_min = re.search(r'(\d+) mins', time_str)
         if match_min:
             minutes = int(match_min.group(1))
+        return hours, minutes
 
-        # Convert to HH:mm format
-        hh_mm_format = f'{hours:02d}:{minutes:02d}'
-        cooking_times_HH_mm.append(hh_mm_format)
+    # Process cooking times
+    for time_str in cook:
+        hours, minutes = get_hours_and_minutes(time_str)
+        cook_HH_mm.append((hours, minutes))
 
+    # Process prep times
+    for time_str in prep:
+        hours, minutes = get_hours_and_minutes(time_str)
+        prep_HH_mm.append((hours, minutes))
 
-    # Initialize variables to store total hours and total minutes
-    total_hours = 0
-    total_minutes = 0
+    # Calculate the average cooking time
+    avg_cook_hours = sum(hours for hours, _ in cook_HH_mm) / max(len(cook_HH_mm), 1)
+    avg_cook_minutes = sum(minutes for _, minutes in cook_HH_mm) / max(len(cook_HH_mm), 1)
 
-    # Iterate through the list and add up the hours and minutes
-    for hh_mm_format in cooking_times_HH_mm:
-        hours, minutes = map(int, hh_mm_format.split(':'))
-        total_hours += hours
-        total_minutes += minutes
+    # Calculate the average prep time
+    avg_prep_hours = sum(hours for hours, _ in prep_HH_mm) / max(len(prep_HH_mm), 1)
+    avg_prep_minutes = sum(minutes for _, minutes in prep_HH_mm) / max(len(prep_HH_mm), 1)
 
-    # Calculate the total hours and minutes
+    # Calculate total hours and minutes
+    total_hours = int(avg_cook_hours + avg_prep_hours)
+    total_minutes = int(avg_cook_minutes + avg_prep_minutes)
+
+    # Calculate carryover from minutes to hours
     total_hours += total_minutes // 60
     total_minutes %= 60
 
