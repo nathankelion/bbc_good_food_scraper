ef7959934dd9b7f92d6b48bcec457a73dbdfc463
nathankelion <nathankelion26@gmail.com>
2023-09-21 13:59:59 +0100

    modified for database connection

--- sql_code/create_tables.sql
+++ sql_code/create_tables.sql
@@ -1,28 +1,34 @@
 USE BBC_Good_Food
 GO
 
+-- Drop tables if they exist
+DROP TABLE IF EXISTS nutrition
+GO
+DROP TABLE IF EXISTS recipe_info
+GO
+
 -- Create the recipe_info table
 CREATE TABLE recipe_info (
     recipe_id		INT PRIMARY KEY NOT NULL,
-    [Recipe Name]	VARCHAR(75),
-    [Recipe Links]	VARCHAR(100) NOT NULL,
-    [Cooking Time]	TIME,
-    [Heath Status]	VARCHAR(15),
-	[Diet]			VARCHAR(15),
-	[Difficulty]	VARCHAR(15)
+    RecipeName		VARCHAR(75),
+    RecipeLink		VARCHAR(100) NOT NULL,
+    CookingTime		TIME(0),
+    HealthStatus	VARCHAR(15),
+	Diet			VARCHAR(15),
+	Difficulty		VARCHAR(15)
 )
 GO
 
 -- Create the nutrition table
 CREATE TABLE nutrition (
 	recipe_id		INT NOT NULL,
-	[kcal]			INT,
-	[Fat (g)]		FLOAT,
-	[Saturates (g)] FLOAT,
-	[Carbs (g)]		FLOAT,
-	[Sugars (g)]	FLOAT,
-	[Fibre (g)]		FLOAT,
-	[Protein (g)]	FLOAT,
-	[Salt (g)]		FLOAT,
+	kcal			INT,
+	[Fat(g)]		FLOAT,
+	[Saturates(g)]	FLOAT,
+	[Carbs(g)]		FLOAT,
+	[Sugars(g)]		FLOAT,
+	[Fibre(g)]		FLOAT,
+	[Protein(g)]	FLOAT,
+	[Salt(g)]		FLOAT,
 	FOREIGN KEY (recipe_id) REFERENCES recipe_info(recipe_id)
 );
