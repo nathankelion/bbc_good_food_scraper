744c058502789481d17ecbc82e65926b384a7885
nathankelion <nathankelion26@gmail.com>
2023-10-27 18:32:34 +0100

    updated scraper

--- recipe_scraping_script.py
+++ recipe_scraping_script.py
@@ -233,7 +233,7 @@ db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 
 # Create a SQLAlchemy engine
-connection_string = f"mssql+pyodbc://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?driver=ODBC+Driver+17+for+SQL+Server"
+connection_string = f"mssql+pyodbc://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?driver=ODBC+Driver+17+for+SQL+Server&fast_executemany=True"
 engine = create_engine(connection_string)
 Conn = engine.connect()
 
