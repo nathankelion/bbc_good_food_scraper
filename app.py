0c66e5323c13c1cdd3006fb4b21b71ae3faef7c5
nathankelion <nathankelion26@gmail.com>
2024-01-02 12:57:43 +0000

    working nutrition filter system

--- app.py
+++ app.py
@@ -12,18 +12,6 @@ st.set_page_config(layout="wide")
 st.title('BBC Good Food Recipe Finder')
 
 # Define the query and call the SQL_query function to get the DataFrame
-# query = text('''
-#         SELECT DISTINCT ri.RecipeName, ri.CookingTime, ri.HealthStatus, ri.Diet, ri.Difficulty, n.kcal, n.[Fat(g)], n.[Saturates(g)], n.[Carbs(g)], n.[Sugars(g)], n.[Fibre(g)], n.[Protein(g)], n.[Salt(g)], ri.RecipeLink, Ingredient
-#         FROM recipe_info    AS ri
-#         JOIN nutrition      AS n ON ri.recipe_id = n.recipe_id
-#         JOIN ingredients    AS i ON ri.recipe_id = i.recipe_id
-#     ''')
-
-# full_df = SQL_query(query)
-
-# Refine the results 
-# result_df = full_df.drop(columns=['Ingredient']).drop_duplicates().reset_index(drop=True)
-
 query = text('''
         SELECT DISTINCT ri.RecipeName, ri.CookingTime, ri.HealthStatus, ri.Diet, ri.Difficulty, n.kcal, n.[Fat(g)], n.[Saturates(g)], n.[Carbs(g)], n.[Sugars(g)], n.[Fibre(g)], n.[Protein(g)], n.[Salt(g)], ri.RecipeLink
         FROM recipe_info    AS ri
@@ -60,22 +48,22 @@ health_status = st.sidebar.selectbox('Health Status', ["I don't mind",'Healthy']
 # Diet drop-down
 selected_diet = st.sidebar.selectbox('Select Diet', ["I don't mind", 'Regular', 'Vegetarian (inc. Vegan)', 'Vegan'])
 
-# Difficulty tickboxes
+# Difficulty boxes
 selected_difficulties = st.sidebar.multiselect('Select Difficult(y/ies)', ['Not given', 'Easy', 'More effort', 'A challenge'])
 
 # Would you like nutritional sliders to show?
 nutrition_filters = st.sidebar.checkbox('Show/Hide Nutrition Filters')
 
-# Create sliders for filtering by nutrition values
+# Create boxes for filtering by nutrition values
 if nutrition_filters:
-    kcals = st.sidebar.multiselect('How many Calories?', ['< 250 kcal', '300-500 kcal', '500-750 kcal', '750-1000 kcal', '> 1000 kcal'])
-    fat_content = st.sidebar.multiselect('How much Fat?', ['< 10 g', '10-20 g', '20-30 g', '30-40 g', '> 40 g'])
-    saturates_content = st.sidebar.multiselect('How much Saturated Fat?', ['< 2 g', '2-4 g', '4-6 g', '6-8 g', '8-10 g', '> 10 g'])
-    carbs_content = st.sidebar.multiselect('How many Carbs?', ['< 15 g', '15-30 g', '30-45 g', '45-60 g', '60-75 g', '> 75 g'])
-    sugars_content = st.sidebar.multiselect('How much Sugars?', ['< 5 g', '5-10 g', '10-15 g', '15-20 g', '> 20 g'])
-    fibre_content = st.sidebar.multiselect('How much Dietary Fiber?', ['< 2 g', '2-4 g', '4-6 g', '6-8 g', '8-10 g', '> 10 g'])
-    protein_content = st.sidebar.multiselect('How much Protein?', ['< 10 g', '10-20 g', '20-30 g',' 30-40 g', '40-50 g', '> 50 g'])
-    salt_content = st.sidebar.multiselect('How much Salt?', ['< 1 g', '1-2 g', '2-3 g', '3-4 g', '> 4 g'])
+    kcals = st.sidebar.multiselect('How many Calories?', ['<= 250 kcal', '250-500 kcal', '500-750 kcal', '750-1000 kcal', '> 1000 kcal'])
+    fat_content = st.sidebar.multiselect('How much Fat?', ['<= 10 g', '10-20 g', '20-30 g', '30-40 g', '> 40 g'])
+    saturates_content = st.sidebar.multiselect('How much Saturated Fat?', ['<= 2 g', '2-4 g', '4-6 g', '6-8 g', '8-10 g', '> 10 g'])
+    carbs_content = st.sidebar.multiselect('How many Carbs?', ['<= 15 g', '15-30 g', '30-45 g', '45-60 g', '60-75 g', '> 75 g'])
+    sugars_content = st.sidebar.multiselect('How much Sugars?', ['<= 5 g', '5-10 g', '10-15 g', '15-20 g', '> 20 g'])
+    fibre_content = st.sidebar.multiselect('How much Dietary Fiber?', ['<= 2 g', '2-4 g', '4-6 g', '6-8 g', '8-10 g', '> 10 g'])
+    protein_content = st.sidebar.multiselect('How much Protein?', ['<= 10 g', '10-20 g', '20-30 g','30-40 g', '40-50 g', '> 50 g'])
+    salt_content = st.sidebar.multiselect('How much Salt?', ['<= 1 g', '1-2 g', '2-3 g', '3-4 g', '> 4 g'])
 
 # Add a "Remove All Filters" button
 if st.sidebar.button('Remove All Filters'):
@@ -145,8 +133,146 @@ if selected_difficulties:
     result_df = result_df[result_df['Difficulty'].isin(selected_difficulties)]
 
 # Apply Nutrition filters
-# if nutrition_filters:
-    
+if nutrition_filters:
+    # Filter based on Calories
+    kcal_filters = {
+    '<= 250 kcal': result_df['kcal'] <= 250,
+    '250-500 kcal': (result_df['kcal'] > 250) & (result_df['kcal'] <= 500),
+    '500-750 kcal': (result_df['kcal'] > 500) & (result_df['kcal'] <= 750),
+    '750-1000 kcal': (result_df['kcal'] > 750) & (result_df['kcal'] <= 1000),
+    '> 1000 kcal': result_df['kcal'] > 1000
+    }
+
+    if kcals:
+        kcal_filter = pd.Series(data=False, index=result_df.index)
+        for selected_kcal in kcals:
+            kcal_filter |= kcal_filters[selected_kcal]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[kcal_filter]
+
+    # Filter based on Fat content
+    fat_filters = {
+        '<= 10 g': result_df['Fat(g)'] <= 10,
+        '10-20 g': (result_df['Fat(g)'] > 10) & (result_df['Fat(g)'] <= 20),
+        '20-30 g': (result_df['Fat(g)'] > 20) & (result_df['Fat(g)'] <= 30),
+        '30-40 g': (result_df['Fat(g)'] > 30) & (result_df['Fat(g)'] <= 40),
+        '> 40 g': result_df['Fat(g)'] > 40
+    }
+
+    if fat_content:
+        fat_filter = pd.Series(data=False, index=result_df.index)
+        for selected_fat in fat_content:
+            fat_filter |= fat_filters[selected_fat]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[fat_filter]
+
+    # Filter based on Saturated Fat content
+    saturates_filters = {
+        '<= 2 g': result_df['Saturates(g)'] <= 2,
+        '2-4 g': (result_df['Saturates(g)'] > 2) & (result_df['Saturates(g)'] <= 4),
+        '4-6 g': (result_df['Saturates(g)'] > 4) & (result_df['Saturates(g)'] <= 6),
+        '6-8 g': (result_df['Saturates(g)'] > 6) & (result_df['Saturates(g)'] <= 8),
+        '8-10 g': (result_df['Saturates(g)'] > 8) & (result_df['Saturates(g)'] <= 10),
+        '> 10 g': result_df['Saturates(g)'] > 10
+    }
+
+    if saturates_content:
+        saturates_filter = pd.Series(data=False, index=result_df.index)
+        for selected_saturates in saturates_content:
+            saturates_filter |= saturates_filters[selected_saturates]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[saturates_filter]
+
+    # Filter based on Carbs content
+    carbs_filters = {
+        '<= 15 g': result_df['Carbs(g)'] <= 15,
+        '15-30 g': (result_df['Carbs(g)'] > 15) & (result_df['Carbs(g)'] <= 30),
+        '30-45 g': (result_df['Carbs(g)'] > 30) & (result_df['Carbs(g)'] <= 45),
+        '45-60 g': (result_df['Carbs(g)'] > 45) & (result_df['Carbs(g)'] <= 60),
+        '60-75 g': (result_df['Carbs(g)'] > 60) & (result_df['Carbs(g)'] <= 75),
+        '> 75 g': result_df['Carbs(g)'] > 75
+    }
+
+    if carbs_content:
+        carbs_filter = pd.Series(data=False, index=result_df.index)
+        for selected_carbs in carbs_content:
+            carbs_filter |= carbs_filters[selected_carbs]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[carbs_filter]
+
+    # Filter based on Sugars content
+    sugars_filters = {
+        '<= 5 g': result_df['Sugars(g)'] <= 5,
+        '5-10 g': (result_df['Sugars(g)'] > 5) & (result_df['Sugars(g)'] <= 10),
+        '10-15 g': (result_df['Sugars(g)'] > 10) & (result_df['Sugars(g)'] <= 15),
+        '15-20 g': (result_df['Sugars(g)'] > 15) & (result_df['Sugars(g)'] <= 20),
+        '> 20 g': result_df['Sugars(g)'] > 20
+    }
+
+    if sugars_content:
+        sugars_filter = pd.Series(data=False, index=result_df.index)
+        for selected_sugars in sugars_content:
+            sugars_filter |= sugars_filters[selected_sugars]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[sugars_filter]
+
+    # Filter based on Dietary Fiber content
+    fibre_filters = {
+        '<= 2 g': result_df['Fibre(g)'] <= 2,
+        '2-4 g': (result_df['Fibre(g)'] > 2) & (result_df['Fibre(g)'] <= 4),
+        '4-6 g': (result_df['Fibre(g)'] > 4) & (result_df['Fibre(g)'] <= 6),
+        '6-8 g': (result_df['Fibre(g)'] > 6) & (result_df['Fibre(g)'] <= 8),
+        '8-10 g': (result_df['Fibre(g)'] > 8) & (result_df['Fibre(g)'] <= 10),
+        '> 10 g': result_df['Fibre(g)'] > 10
+    }
+
+    if fibre_content:
+        fibre_filter = pd.Series(data=False, index=result_df.index)
+        for selected_fibre in fibre_content:
+            fibre_filter |= fibre_filters[selected_fibre]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[fibre_filter]
+
+    # Filter based on Protein content
+    protein_filters = {
+        '<= 10 g': result_df['Protein(g)'] <= 10,
+        '10-20 g': (result_df['Protein(g)'] > 10) & (result_df['Protein(g)'] <= 20),
+        '20-30 g': (result_df['Protein(g)'] > 20) & (result_df['Protein(g)'] <= 30),
+        '30-40 g': (result_df['Protein(g)'] > 30) & (result_df['Protein(g)'] <= 40),
+        '40-50 g': (result_df['Protein(g)'] > 40) & (result_df['Protein(g)'] <= 50),
+        '> 50 g': result_df['Protein(g)'] > 50
+    }
+
+    if protein_content:
+        protein_filter = pd.Series(data=False, index=result_df.index)
+        for selected_protein in protein_content:
+            protein_filter |= protein_filters[selected_protein]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[protein_filter]
+
+    # Filter based on Salt content
+    salt_filters = {
+        '<= 1 g': result_df['Salt(g)'] <= 1,
+        '1-2 g': (result_df['Salt(g)'] > 1) & (result_df['Salt(g)'] <= 2),
+        '2-3 g': (result_df['Salt(g)'] > 2) & (result_df['Salt(g)'] <= 3),
+        '3-4 g': (result_df['Salt(g)'] > 3) & (result_df['Salt(g)'] <= 4),
+        '> 4 g': result_df['Salt(g)'] > 4
+    }
+
+    if salt_content:
+        salt_filter = pd.Series(data=False, index=result_df.index)
+        for selected_salt in salt_content:
+            salt_filter |= salt_filters[selected_salt]
+
+        # Apply the combined filter to the DataFrame
+        result_df = result_df[salt_filter]
 
 
 # Display subtitle
